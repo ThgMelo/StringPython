1950575bcd00ef0a3c28c94a57173d2c15f94e8e
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-05 09:19:29 -0300

    feat: Fatiamento de strings

--- main.py
+++ main.py
@@ -1,2 +1,10 @@
-url = "http://bytebank.com/cambio?moedaOrigem=real&moedaDestino=dolar&quantidade=100"
+url = "bytebank.com/cambio?moedaOrigem=real"
 print(url)
+
+url_base = url[0:19]
+print(url_base)
+
+url_parametros = url[20:36]
+print(url_parametros)
+
+print(url) # string original não é alterada - strings são imutáveis em Python
