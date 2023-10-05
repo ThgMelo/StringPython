032033a2cf342c0db6d3a5e7f55e999916e76866
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-05 11:00:04 -0300

    refact: RegEx

--- extrator_cep.py
+++ extrator_cep.py
@@ -3,7 +3,7 @@ endereco = "Rua das Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ
 import re # Regular Expression -- RegEx
 # 5 dígitos + hífen (opcional) + 3 dígitos
 
-padrao = re.compile("[0987654321][0987654321][0987654321][0987654321][0987654321][-]?[0987654321][0987654321][0987654321]")
+padrao = re.compile("[0-9]{5}[-]?[0-9]{3}")
 busca = padrao.search(endereco) # Retorna um objeto Match
 if busca:
     cep = busca.group()
