b7f784fed73ce353efbdfc3f26da36cceb521a00
Thiago Melo <thiago.melo@alfalaval.com>
2023-10-05 11:37:14 -0300

    feat: método __len__()

--- extrator_url.py
+++ extrator_url.py
@@ -42,9 +42,14 @@ class ExtratorURL:
         else:
             valor = self.get_url_parametros()[indice_valor:indice_e_comercial]
         return valor
+    
+    def __len__(self):
+        return len(self.url)
 
 
 url = "https://bytebank.com/cambio?moedaDestino=dolar&quantidade=100&moedaOrigem=real"
 extrator_url = ExtratorURL(url)
 valor_quantidade = extrator_url.get_valor_parametro("quantidade")
 print(valor_quantidade)
+
+print(f'O tamanho da URL: {len(extrator_url)}')
